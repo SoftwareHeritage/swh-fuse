c82f17e3990ccc209b69002684600cd1b85438d1
Thibault Allançon <haltode@gmail.com>
2020-10-12 15:22:03 +0200

    tests: add unit tests for submodules

--- swh/fuse/tests/data/config.py
+++ swh/fuse/tests/data/config.py
@@ -9,11 +9,21 @@
 REGULAR_FILE = "swh:1:cnt:61d3c9e1157203f0c4ed5165608d92294eaca808"
 # Directory
 ROOT_DIR = "swh:1:dir:c6dcbe9711ea6d5a31429a833a3d0c59cbbb2578"
+DIR_WITH_SUBMODULES = "swh:1:dir:80ae84abc6122c47aae597fde99645f8663d1aba"
 # Revision
 ROOT_REV = "swh:1:rev:b8cedc00407a4c56a3bda1ed605c6fc166655447"
+SUBMODULES = [
+    "swh:1:rev:87dd6843678575f8dda962f239d14ef4be14b352",
+    "swh:1:rev:1a2390247ad6d08160e0dd74f40a01a9578659c2",
+    "swh:1:rev:4d78994915af1bde9a95c04a8c27d8dca066232a",
+    "swh:1:rev:3e6e1001dc6e095dbd5c88005e80969f60e384e1",
+    "swh:1:rev:11e893fc1357bc688418ddf1087c2b7aa25d154d",
+    "swh:1:rev:1c2bd024d13f8011307e13386cf1fea2180352b5",
+    "swh:1:rev:92baf7293dd2d418d2ac4b141b0faa822075d9f7",
+]
 # Release
 # TODO
 # Snapshot
 # TODO
 
-ALL_ENTRIES = [REGULAR_FILE, ROOT_DIR, ROOT_REV]
+ALL_ENTRIES = [REGULAR_FILE, ROOT_DIR, DIR_WITH_SUBMODULES, ROOT_REV, *SUBMODULES]
--- swh/fuse/tests/test_directory.py
+++ swh/fuse/tests/test_directory.py
@@ -1,8 +1,8 @@
-from os import listdir
+from os import listdir, readlink
 from pathlib import Path
 
 from swh.fuse.tests.common import get_data_from_archive
-from swh.fuse.tests.data.config import ROOT_DIR
+from swh.fuse.tests.data.config import DIR_WITH_SUBMODULES, ROOT_DIR
 
 
 def test_list_dir(fuse_mntdir):
@@ -21,3 +21,19 @@ def test_access_file(fuse_mntdir):
 def test_access_subdir(fuse_mntdir):
     dir_path = Path(fuse_mntdir, "archive", ROOT_DIR, "src")
     assert dir_path.is_dir()
+
+
+def test_access_submodule_entries(fuse_mntdir):
+    dir_path = Path(fuse_mntdir, "archive", DIR_WITH_SUBMODULES)
+    submodules = {
+        "book": "swh:1:rev:87dd6843678575f8dda962f239d14ef4be14b352",
+        "edition-guide": "swh:1:rev:1a2390247ad6d08160e0dd74f40a01a9578659c2",
+        "embedded-book": "swh:1:rev:4d78994915af1bde9a95c04a8c27d8dca066232a",
+        "nomicon": "swh:1:rev:3e6e1001dc6e095dbd5c88005e80969f60e384e1",
+        "reference": "swh:1:rev:11e893fc1357bc688418ddf1087c2b7aa25d154d",
+        "rust-by-example": "swh:1:rev:1c2bd024d13f8011307e13386cf1fea2180352b5",
+        "rustc-guide": "swh:1:rev:92baf7293dd2d418d2ac4b141b0faa822075d9f7",
+    }
+    for filename, swhid in submodules.items():
+        target = f"../../archive/{swhid}"
+        assert readlink(Path(dir_path, filename)) == target
