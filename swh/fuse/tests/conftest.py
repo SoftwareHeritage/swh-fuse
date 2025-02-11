6339d76a33c53f4a50186c00dd454a226bd34f6e
Martin Kirchgessner <martin.kirchgessner@inria.fr>
2025-02-11 17:33:22 +0100

    run with Popen so can actually see its log

--- swh/fuse/tests/conftest.py
+++ swh/fuse/tests/conftest.py
@@ -4,10 +4,10 @@
 # See top-level LICENSE file for more information
 
 import json
-from multiprocessing import Process
 import os
 from pathlib import Path
-from tempfile import NamedTemporaryFile, TemporaryDirectory
+import subprocess
+import tempfile
 import time
 
 from click.testing import CliRunner
@@ -36,49 +36,58 @@ def web_api_mock(requests_mock):
 
 @pytest.fixture
 def fuse_mntdir(web_api_mock):
-    tmpdir = TemporaryDirectory(suffix=".swh-fuse-test")
+    # fmt: off
+    with (
+        tempfile.TemporaryDirectory(suffix=".swh-fuse-test") as tmpdir,
+        tempfile.NamedTemporaryFile(suffix=".swh-fuse-test.yml") as tmpfile
+    ):
+        # fmt: on
 
-    config = {
-        "cache": {
-            "metadata": {"in-memory": True},
-            "blob": {"in-memory": True},
-        },
-        "web-api": {"url": API_URL, "auth-token": None},
-        "json-indent": None,
-    }
+        config = {
+            "cache": {
+                "metadata": {"in-memory": True},
+                "blob": {"in-memory": True},
+            },
+            "web-api": {"url": API_URL, "auth-token": None},
+            "json-indent": None,
+        }
+        config_path = Path(tmpfile.name)
+        config_path.write_text(yaml.dump(config))
 
-    # Run FUSE in foreground mode but in a separate process, so it does not
-    # block execution and remains easy to kill during teardown
-    def fuse_process(mntdir: Path):
-        with NamedTemporaryFile(suffix=".swh-fuse-test.yml") as tmpfile:
-            config_path = Path(tmpfile.name)
-            config_path.write_text(yaml.dump(config))
-            CliRunner().invoke(
-                cli.fuse,
-                args=[
-                    "--config-file",
-                    str(config_path),
-                    "mount",
-                    str(mntdir),
-                    "--foreground",
-                ],
-            )
+        # Run FUSE in foreground mode but in a separate process, so it does not
+        # block execution and remains easy to kill during teardown
+        fuse = subprocess.Popen(
+            [
+                "swh",
+                "--log-level",
+                "swh.fuse:DEBUG",
+                "fs",
+                "--config-file",
+                tmpfile.name,
+                "mount",
+                tmpdir,
+                "--foreground",
+            ],
+        )
 
-    fuse = Process(target=fuse_process, args=[Path(tmpdir.name)])
-    fuse.start()
-    # Wait max 3 seconds for the FUSE to correctly mount
-    for i in range(30):
         try:
-            root = os.listdir(tmpdir.name)
-            if root:
-                break
-        except FileNotFoundError:
-            pass
-        time.sleep(0.1)
-    else:
-        raise FileNotFoundError(f"Could not mount FUSE in {tmpdir.name}")
+            # Wait max 5 seconds for the FUSE to correctly mount
+            for i in range(50):
+                try:
+                    root = os.listdir(tmpdir)
+                    if root:
+                        break
+                except FileNotFoundError:
+                    pass
+                time.sleep(0.1)
+            else:
+                raise FileNotFoundError(f"Could not mount FUSE in {tmpdir}")
 
-    yield Path(tmpdir.name)
+            yield Path(tmpdir)
 
-    CliRunner().invoke(cli.umount, [tmpdir.name])
-    fuse.join()
+            CliRunner().invoke(cli.umount, [tmpdir])
+            fuse.wait(3)
+        finally:
+            fuse.poll()
+            if fuse.returncode is None:
+                fuse.kill()
